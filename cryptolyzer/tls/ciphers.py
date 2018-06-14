00bc4476f9982a7967004160327e1d537593834f
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-06-14 18:46:46 +0200

    tls: Add cipher suite preference to cipher plugin

--- cryptolyzer/tls/ciphers.py
+++ cryptolyzer/tls/ciphers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict
+
 import json
 
 from cryptoparser.common.exception import NetworkError, NetworkErrorType
@@ -14,20 +16,11 @@ from cryptolyzer.common.analyzer import AnalyzerTls, AnalyzerResultTls
 from cryptolyzer.tls.versions import AnalyzerVersions
 
 
-class AnalyzerResultCipherSuites(AnalyzerResultTls):
-    def __init__(self):
-        self.cipher_suites = {}
-
-    def add_cipher_suites(self, tls_version, cipher_suites):
-        self.cipher_suites[tls_version] = cipher_suites
-
-    def as_json(self):
-        return json.dumps({
-            'cipher_suites': {
-                tls_version.name: [cipher_suite.name for cipher_suite in supported_cipher_suites]
-                for tls_version, supported_cipher_suites in self.cipher_suites.items()
-            }
-        })
+class AnalyzerResultCipherSuites(AnalyzerResultBase):
+    def __init__(self, protocol_version, cipher_suites, cipher_suite_preference):
+        self.protocol_version = protocol_version
+        self.cipher_suites = cipher_suites
+        self.cipher_suite_preference = cipher_suite_preference
 
 
 class AnalyzerCipherSuites(AnalyzerTls):
@@ -39,47 +32,59 @@ class AnalyzerCipherSuites(AnalyzerTls):
     def get_help(cls):
         return 'Check which cipher suites supported by the server(s)'
 
-    def analyze(self, l7_client):
-        result = AnalyzerResultCipherSuites()
-        for tls_version in AnalyzerVersions().analyze(l7_client).versions:
-            accepted_cipher_suites = []
-            if tls_version in TlsVersion:
-                remaining_cipher_suites = list(TlsCipherSuite)
-            else:
-                remaining_cipher_suites = list(SslCipherKind)
+    def _get_accepted_cipher_suites(self, l7_client, protocol_version, checkable_cipher_suites):
+        accepted_cipher_suites = []
+        remaining_cipher_suites = list(checkable_cipher_suites)
 
-            while True:
-                try:
-                    if tls_version in TlsVersion:
-                        client_hello = TlsHandshakeClientHelloAnyAlgorithm(l7_client.host)
-                        client_hello.cipher_suites = TlsCipherSuiteVector(remaining_cipher_suites)
-                        client_hello.protocol_version = TlsProtocolVersionFinal(tls_version)
+        while True:
+            try:
+                if protocol_version in TlsVersion:
+                    client_hello = TlsHandshakeClientHelloAnyAlgorithm(l7_client.host)
+                    client_hello.cipher_suites = TlsCipherSuiteVector(remaining_cipher_suites)
+                    client_hello.protocol_version = TlsProtocolVersionFinal(protocol_version)
 
-                        server_messages = l7_client.do_tls_handshake(client_hello, client_hello.protocol_version)
+                    server_messages = l7_client.do_tls_handshake(client_hello, client_hello.protocol_version)
 
-                        server_cipher_suite = server_messages[TlsHandshakeType.SERVER_HELLO].cipher_suite
-                    else:
-                        client_hello = SslHandshakeClientHelloAnyAlgorithm()
-                        client_hello.cipher_suites = TlsCipherSuiteVector(remaining_cipher_suites)
-                        server_messages = l7_client.do_ssl_handshake(client_hello)
-
-                        accepted_cipher_suites = server_messages[SslMessageType.SERVER_HELLO].cipher_kinds
-                        break
-
+                    server_cipher_suite = server_messages[TlsHandshakeType.SERVER_HELLO].cipher_suite
                     for index, cipher_suite in enumerate(remaining_cipher_suites):
                         if cipher_suite == server_cipher_suite:
                             del remaining_cipher_suites[index]
                             accepted_cipher_suites.append(cipher_suite)
                             break
-                except TlsAlert as e:
-                    if e.description == TlsAlertDescription.HANDSHAKE_FAILURE:
-                        break
-                    else:
-                        raise e
-                except NetworkError as e:
-                    if e.error == NetworkErrorType.NO_RESPONSE:
-                        break
+                else:
+                    client_hello = SslHandshakeClientHelloAnyAlgorithm()
+                    client_hello.cipher_suites = TlsCipherSuiteVector(remaining_cipher_suites)
+                    server_messages = l7_client.do_ssl_handshake(client_hello)
 
-            result.add_cipher_suites(tls_version, accepted_cipher_suites)
+                    accepted_cipher_suites = server_messages[SslMessageType.SERVER_HELLO].cipher_kinds
+                    break
+            except TlsAlert as e:
+                if e.description == TlsAlertDescription.HANDSHAKE_FAILURE:
+                    break
+                else:
+                    raise e
+            except NetworkError as e:
+                if e.error == NetworkErrorType.NO_RESPONSE:
+                    break
+                else:
+                    raise e
+            finally:
+                if not remaining_cipher_suites:
+                    break
 
-        return result
+        return accepted_cipher_suites
+
+    def analyze(self, l7_client, protocol_version):
+        if protocol_version in TlsVersion:
+            checkable_cipher_suites = list(TlsCipherSuite)
+        else:
+            checkable_cipher_suites = list(SslCipherKind)
+
+        accepted_cipher_suites = self._get_accepted_cipher_suites(l7_client, protocol_version, checkable_cipher_suites)
+        if len(accepted_cipher_suites) > 1:
+            checkable_cipher_suites = [accepted_cipher_suites[-1], accepted_cipher_suites[0]]
+            cipher_suite_preference = self._get_accepted_cipher_suites(l7_client, protocol_version, checkable_cipher_suites) != checkable_cipher_suites
+        else:
+            cipher_suite_preference = None
+
+        return AnalyzerResultCipherSuites(protocol_version, accepted_cipher_suites, cipher_suite_preference)
