ba39838ea619ad15934076a951337895c77a03cf
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-06-15 18:22:14 +0200

    tls: Add SSLv2 support

--- cryptolyzer/tls/versions.py
+++ cryptolyzer/tls/versions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+
 from cryptoparser.common.exception import NetworkError, NetworkErrorType
 
 from cryptoparser.tls.client import TlsHandshakeClientHelloAnyAlgorithm, TlsAlert
+from cryptoparser.tls.client import SslHandshakeClientHelloAnyAlgorithm, SslError
 from cryptoparser.tls.subprotocol import TlsHandshakeType, TlsAlertDescription
-from cryptoparser.tls.version import TlsVersion, TlsProtocolVersionFinal
+from cryptoparser.tls.subprotocol import SslMessageType, SslErrorType
+from cryptoparser.tls.version import TlsVersion, TlsProtocolVersionFinal, SslProtocolVersion
 
-from cryptolyzer.common.analyzer import AnalyzerBase, AnalyzerResultBase
+from cryptolyzer.common.analyzer import AnalyzerTls, AnalyzerResultTls
 
 
-class AnalyzerResultVersions(AnalyzerResultBase):  # pylint: disable=too-few-public-methods
+class AnalyzerResultVersions(AnalyzerResultTls):  # pylint: disable=too-few-public-methods
     def __init__(self, versions):
         super(AnalyzerResultVersions, self).__init__()
 
@@ -20,7 +24,7 @@ class AnalyzerResultVersions(AnalyzerResultBase):  # pylint: disable=too-few-pub
         return json.dumps([version.name for version in self.versions])
 
 
-class AnalyzerVersions(AnalyzerBase):
+class AnalyzerVersions(AnalyzerTls):
     @classmethod
     def get_name(cls):
         return 'versions'
@@ -31,6 +35,19 @@ class AnalyzerVersions(AnalyzerBase):
 
     def analyze(self, l7_client):
         supported_protocols = []
+
+        try:
+            client_hello = SslHandshakeClientHelloAnyAlgorithm()
+            server_messages = l7_client.do_ssl_handshake(client_hello)
+        except SslError as e:
+            if e.error != SslErrorType.NO_CIPHER_ERROR:
+                raise e
+        except NetworkError as e:
+            if e.error != NetworkErrorType.NO_RESPONSE:
+                raise e
+        else:
+            if server_messages[SslMessageType.SERVER_HELLO].cipher_kinds:
+                supported_protocols.append(SslVersion.SSL2)
 
         client_hello = TlsHandshakeClientHelloAnyAlgorithm(l7_client.host)
         for tls_version in TlsVersion:
@@ -46,6 +63,6 @@ class AnalyzerVersions(AnalyzerBase):
                     raise e
             else:
                 if server_messages[TlsHandshakeType.SERVER_HELLO].protocol_version == protocol_version:
-                    supported_protocols.append(protocol_version)
+                    supported_protocols.append(protocol_version.minor)
 
         return AnalyzerResultVersions(supported_protocols)
