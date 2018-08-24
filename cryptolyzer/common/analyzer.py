1cffa80b6123407eb15fc7a3ee8d275dbc1665ee
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-08-24 22:37:35 +0200

    tls: Initial commit of DH paramater (key size) check

--- cryptolyzer/common/analyzer.py
+++ cryptolyzer/common/analyzer.py
@@ -84,7 +84,7 @@ class AnalyzerTlsBase(object):
         raise NotImplementedError()
 
 
-from cryptolyzer.tls import ciphers, pubkeys, curves, sigalgos, versions  # noqa: F401, pylint: disable=unused-import
+from cryptolyzer.tls import ciphers, pubkeys, curves, sigalgos, versions, dhparams  # noqa: F401, pylint: disable=unused-import
 from cryptoparser.tls.client import L7ClientTls  # noqa: F401
 
 from cryptoparser.tls.version import TlsProtocolVersionFinal, TlsVersion, SslProtocolVersion  # noqa: F401
@@ -148,6 +148,7 @@ class ProtocolHandlerTls10(ProtocolHandlerTlsExactVersion):
     def get_analyzers(cls):
         return ProtocolHandlerSsl3.get_analyzers() + [
             curves.AnalyzerCurves,
+            dhparams.AnalyzerDHParams,
         ]
 
     @classmethod
--- cryptolyzer/common/result.py
+++ cryptolyzer/common/result.py
@@ -10,3 +10,21 @@ class AnalyzerResultBase(JSONSerializable):
 
 class AnalyzerResultTls(AnalyzerResultBase):
     pass
+
+
+class DHParameter(JSONSerializable):
+    def __init__(self, public_key):
+        self.public_key = public_key
+
+    @property
+    def key_size(self):
+        return self.public_key.key_size
+
+    def as_json(self):
+        result = {'key_size': self.key_size}
+        result.update({
+            key: value
+            for key, value in self.__dict__.items()
+            if key != 'public_key'
+        })
+        return result
