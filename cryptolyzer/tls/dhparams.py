5e094a920f2b9a28718eb21207e862d93028be6c
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-09-02 21:52:35 +0200

    tls: Implement parameter reuse check for DH parameters

--- cryptolyzer/tls/dhparams.py
+++ cryptolyzer/tls/dhparams.py
@@ -29,20 +29,27 @@ class AnalyzerDHParams(AnalyzerTlsBase):
         client_hello = TlsHandshakeClientHelloKeyExchangeDHE(l7_client.host)
 
         dhparams = []
-        try:
-            server_messages = l7_client.do_tls_handshake(
-                client_hello,
-                client_hello.protocol_version,
-                TlsHandshakeType.SERVER_KEY_EXCHANGE)
-        except TlsAlert:
-            pass
-        except NetworkError as e:
-            if e.error != NetworkErrorType.NO_RESPONSE:
-                raise e
-        else:
-            server_key_exchange_message = server_messages[TlsHandshakeType.SERVER_KEY_EXCHANGE]
-            dh_public_key = parse_dh_params(server_key_exchange_message.param_bytes)
+        dh_public_keys = []
+        for _ in (1, 2):
+            try:
+                server_messages = l7_client.do_tls_handshake(
+                    client_hello,
+                    client_hello.protocol_version,
+                    TlsHandshakeType.SERVER_KEY_EXCHANGE)
+            except TlsAlert:
+                pass
+            except NetworkError as e:
+                if e.error != NetworkErrorType.NO_RESPONSE:
+                    raise e
+            else:
+                server_key_exchange_message = server_messages[TlsHandshakeType.SERVER_KEY_EXCHANGE]
+                dh_public_key = parse_dh_params(server_key_exchange_message.param_bytes)
+                dh_public_keys.append(dh_public_key)
 
-            dhparams.append(DHParameter(dh_public_key))
+                if len(dh_public_keys) == 2:
+                    dhparams.append(DHParameter(
+                        dh_public_keys[0],
+                        dh_public_keys[0].public_numbers().y == dh_public_keys[1].public_numbers(),
+                    ))
 
         return AnalyzerResultDHParams(dhparams)
