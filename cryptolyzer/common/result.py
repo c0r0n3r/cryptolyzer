dc9c9e69f21a00d24f8f2920d95e5e59476b000e
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2018-09-02 21:43:28 +0200

    tls: Implement prime (safety) check for DH parameters

--- cryptolyzer/common/result.py
+++ cryptolyzer/common/result.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from cryptography.hazmat.backends import default_backend  # pylint: disable=import-error
+
 from cryptoparser.common.base import JSONSerializable
 
 
@@ -17,6 +19,15 @@ class AnalyzerResultTls(AnalyzerResultBase):
 class DHParameter(JSONSerializable):
     def __init__(self, public_key):
         self.public_key = public_key
+
+        codes = default_backend()._ffi.new("int[]", 1)  # pylint: disable=protected-access
+        cryptography_dh_check = default_backend()._lib.Cryptography_DH_check  # pylint: disable=protected-access
+        if cryptography_dh_check(public_key._dh_cdata, codes) == 1:  # pylint: disable=protected-access
+            self.prime = (codes[0] & 0x01) == 0  # DH_CHECK_P_NOT_PRIME
+            self.safe_prime = (codes[0] & 0x02) == 0  # DH_CHECK_P_NOT_SAFE_PRIME
+        else:
+            self.prime = None
+            self.safe_prime = None
 
     @property
     def key_size(self):
