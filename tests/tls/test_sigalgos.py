a9a0574892ca82c92973fa709d90f5b4898fe1ac
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2019-09-25 19:19:44 +0200

    tls/sigalgos: Achieve 100% code coverage

--- tests/tls/test_sigalgos.py
+++ tests/tls/test_sigalgos.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from cryptoparser.tls.subprotocol import TlsAlertDescription
 from cryptoparser.tls.version import TlsVersion, TlsProtocolVersionFinal
 from cryptoparser.tls.extension import TlsSignatureAndHashAlgorithm
 
-from cryptolyzer.tls.client import L7ClientTlsBase
+from cryptolyzer.tls.client import L7ClientTlsBase, TlsAlert
+from cryptolyzer.common.exception import ResponseError, ResponseErrorType
 from cryptolyzer.tls.sigalgos import AnalyzerSigAlgos
 
 from .classes import TestTlsCases
@@ -17,6 +24,34 @@ class TestTlsSigAlgos(TestTlsCases.TestTlsBase):
         l7_client = L7ClientTlsBase.from_scheme('tls', host, port)
         result = analyzer.analyze(l7_client, protocol_version)
         return result
+
+    @mock.patch.object(
+        L7ClientTlsBase, 'do_tls_handshake',
+        side_effect=TlsAlert(TlsAlertDescription.PROTOCOL_VERSION)
+    )
+    def test_error_tls_alert_protocol_version(self, _):
+        result = self.get_result('ecc256.badssl.com', 443)
+        self.assertEqual(result.sig_algos, [])
+
+    @mock.patch.object(
+        L7ClientTlsBase, 'do_tls_handshake',
+        side_effect=[
+            mock.DEFAULT,
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+            ResponseError(ResponseErrorType.UNPARSABLE_RESPONSE),
+        ]
+    )
+    def test_error_response_error_no_response(self, _):
+        result = self.get_result('ecc256.badssl.com', 443)
+        self.assertEqual(result.sig_algos, [
+            TlsSignatureAndHashAlgorithm.DSA_NONE,
+        ])
 
     def test_sigalgos(self):
         result = self.get_result('ecc256.badssl.com', 443)
