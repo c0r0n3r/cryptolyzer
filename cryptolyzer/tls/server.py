8f12a5c06d8027fff37644b1cb03ce853510d255
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2020-07-25 09:48:46 +0200

    tls/server: Move TLS independent logic to separate classes (#28)

--- cryptolyzer/tls/server.py
+++ cryptolyzer/tls/server.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import abc
-import socket
 import attr
 
 import six
 
-from cryptoparser.common.exception import NotEnoughData, InvalidType, InvalidValue
+from cryptoparser.common.exception import NotEnoughData, InvalidValue
 
 from cryptoparser.tls.record import TlsRecord, SslRecord
 from cryptoparser.tls.subprotocol import (
@@ -19,19 +18,14 @@ from cryptoparser.tls.subprotocol import (
     TlsAlertMessage,
     TlsContentType,
     TlsHandshakeType,
-    TlsSubprotocolMessageBase,
 )
 
 from cryptolyzer.common.exception import NetworkError, NetworkErrorType
-from cryptolyzer.common.transfer import L7TransferBase, L4TransferBase, L4ServerTCP
+from cryptolyzer.common.application import L7ServerBase, L7ServerHandshakeBase
 
 
 @attr.s
-class L7ServerTlsBase(L7TransferBase):
-    max_handshake_count = attr.ib(default=None, validator=attr.validators.optional(attr.validators.instance_of(int)))
-    l4_transfer = attr.ib(
-        init=False, default=None, validator=attr.validators.optional(attr.validators.instance_of(L4TransferBase))
-    )
+class L7ServerTlsBase(L7ServerBase):
 
     @classmethod
     @abc.abstractmethod
@@ -42,10 +36,6 @@ class L7ServerTlsBase(L7TransferBase):
     @abc.abstractmethod
     def get_default_port(cls):
         raise NotImplementedError()
-
-    def _init_connection(self):
-        self.l4_transfer = L4ServerTCP(self.address, self.port, self.timeout, self.ip)
-        self.l4_transfer.init_connection()
 
     @staticmethod
     def _get_handshake_class(l4_transfer, fallback_to_ssl):
@@ -82,27 +72,6 @@ class L7ServerTlsBase(L7TransferBase):
 
         return handshake_object.client_messages
 
-    def _do_handshakes(self, last_handshake_message_type, fallback_to_ssl):
-        clients_messages = []
-        actual_handshake_count = 0
-        while True:
-            self.l4_transfer.close()
-
-            if self.max_handshake_count is not None and actual_handshake_count >= self.max_handshake_count:
-                break
-
-            try:
-                self.l4_transfer.accept()
-            except socket.timeout:
-                break
-
-            actual_handshake_count += 1
-            clients_messages.append(self._do_handshake(last_handshake_message_type, fallback_to_ssl))
-
-        self._close_connection()
-
-        return clients_messages
-
     def do_ssl_handshake(self, last_handshake_message_type=SslMessageType.CLIENT_HELLO):
         return self._do_handshakes(
             last_handshake_message_type,
@@ -117,16 +86,12 @@ class L7ServerTlsBase(L7TransferBase):
 
 
 @attr.s
-class TlsServer(object):
-    l4_transfer = attr.ib(validator=attr.validators.instance_of(L4TransferBase))
-    _last_processed_message_type = attr.ib(init=False, default=None)
-    clients_messages = attr.ib(init=False, default={})
-
+class TlsServer(L7ServerHandshakeBase):
     def _is_message_plain_text(self):
         return self.l4_transfer.buffer and self.l4_transfer.buffer_is_plain_text
 
     @abc.abstractmethod
-    def _process_plain_text_message(self):
+    def _parse_record(self):
         raise NotImplementedError()
 
     @abc.abstractmethod
@@ -142,20 +107,11 @@ class TlsServer(object):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def do_handshake(self, last_handshake_message_type):
+    def _process_plain_text_message(self):
         raise NotImplementedError()
 
 
 class TlsServerHandshake(TlsServer):
-    client_messages = attr.ib(
-        init=False,
-        default={},
-        validator=attr.validators.deep_iterable(member_validator=attr.validators.in_(TlsSubprotocolMessageBase))
-    )
-    _last_processed_message_type = attr.ib(
-        init=False, default=None, validator=attr.validators.optional(attr.validators.instance_of(TlsHandshakeType))
-    )
-
     def _process_handshake_message(self, record, last_handshake_message_type):
         for handshake_message in record.messages:
             self._last_processed_message_type = handshake_message.get_handshake_type()
@@ -185,35 +141,11 @@ class TlsServerHandshake(TlsServer):
             TlsAlertMessage(alert_level, alert_description),
         ]).compose())
 
-    def do_handshake(self, last_handshake_message_type=TlsHandshakeType.CLIENT_HELLO):
-        self.client_messages = {}
-        self._last_processed_message_type = None
+    def _parse_record(self):
+        record = TlsRecord.parse_exact_size(self.l4_transfer.buffer)
+        is_handshake = record.content_type == TlsContentType.HANDSHAKE
 
-        while True:
-            try:
-                try:
-                    record = TlsRecord.parse_exact_size(self.l4_transfer.buffer)
-                    self.l4_transfer.flush_buffer()
-                    receivable_byte_num = 0
-                except NotEnoughData as e:
-                    receivable_byte_num = e.bytes_needed
-                except (InvalidType, InvalidValue):
-                    self._process_invalid_message()
-                else:
-                    if record.content_type == TlsContentType.HANDSHAKE:
-                        self._process_handshake_message(record, last_handshake_message_type)
-                    else:
-                        self._process_non_handshake_message(record)
-
-                try:
-                    self.l4_transfer.receive(receivable_byte_num)
-                except NotEnoughData:
-                    self._process_plain_text_message()
-                    raise StopIteration()
-
-                continue
-            except StopIteration:
-                break
+        return record, is_handshake
 
 
 class SslServerHandshake(TlsServer):
@@ -249,36 +181,11 @@ class SslServerHandshake(TlsServer):
     def _handle_error(self, error_type):
         self.l4_transfer.send(SslRecord(SslErrorMessage(error_type)).compose())
 
-    def do_handshake(self, last_handshake_message_type=SslMessageType.CLIENT_HELLO):
-        self.client_messages = {}
-        self._last_processed_message_type = None
+    def _parse_record(self):
+        record = SslRecord.parse_exact_size(self.l4_transfer.buffer)
+        is_handshake = record.message.get_message_type() != SslMessageType.ERROR
 
-        while True:
-            try:
-                try:
-                    record = SslRecord.parse_exact_size(self.l4_transfer.buffer)
-                    self.l4_transfer.flush_buffer()
-                    receivable_byte_num = 0
-                except NotEnoughData as e:
-                    receivable_byte_num = e.bytes_needed
-                except (InvalidType, InvalidValue):
-                    self._process_plain_text_message()
-                    self._process_invalid_message()
-                else:
-                    if record.message.get_message_type() == SslMessageType.ERROR:
-                        self._process_non_handshake_message(record)
-                    else:
-                        self._process_handshake_message(record, last_handshake_message_type)
-
-                try:
-                    self.l4_transfer.receive(receivable_byte_num)
-                except NotEnoughData:
-                    self._process_plain_text_message()
-                    raise StopIteration()
-
-                continue
-            except StopIteration:
-                break
+        return record, is_handshake
 
 
 class L7ServerTls(L7ServerTlsBase):
