60c34500c991c820e349a0f5d027899ba1e7023b
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2020-12-19 22:50:21 +0100

    tls/clientauth: Initial commit of client certificate request analyzer (#36)

---
+++ cryptolyzer/tls/pubkeyreq.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+
+import attr
+
+import asn1crypto.x509
+
+from cryptoparser.tls.extension import TlsSignatureAndHashAlgorithm
+from cryptoparser.tls.subprotocol import (
+    TlsAlertDescription,
+    TlsClientCertificateType,
+    TlsHandshakeType,
+)
+
+from cryptolyzer.common.analyzer import AnalyzerTlsBase
+from cryptolyzer.common.exception import NetworkError, NetworkErrorType, SecurityError
+from cryptolyzer.common.result import AnalyzerResultTls, AnalyzerTargetTls
+from cryptolyzer.tls.client import (
+    TlsHandshakeClientHelloAuthenticationECDSA,
+    TlsHandshakeClientHelloAuthenticationRSA,
+    TlsHandshakeClientHelloAuthenticationRarelyUsed,
+)
+from cryptolyzer.tls.exception import TlsAlert
+
+
+@attr.s
+class AnalyzerResultPublicKeyRequest(AnalyzerResultTls):  # pylint: disable=too-few-public-methods
+    certificate_types = attr.ib(
+        validator=attr.validators.optional(attr.validators.deep_iterable(
+            member_validator=attr.validators.in_(TlsClientCertificateType)
+        ))
+    )
+    supported_signature_algorithms = attr.ib(
+        validator=attr.validators.optional(attr.validators.deep_iterable(
+            member_validator=attr.validators.in_(TlsSignatureAndHashAlgorithm)
+        ))
+    )
+    distinguished_names = attr.ib(
+        validator=attr.validators.optional(attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of(OrderedDict)
+        ))
+    )
+
+
+class AnalyzerPublicKeyRequest(AnalyzerTlsBase):
+    @classmethod
+    def get_name(cls):
+        return 'pubkeyreq'
+
+    @classmethod
+    def get_help(cls):
+        return 'Check whether client public key is required by the server(s)'
+
+    @staticmethod
+    def _analyze_requierd_certificates(analyzable, protocol_version):
+        client_hello_messsages_in_order_of_probability = (
+            TlsHandshakeClientHelloAuthenticationRSA(protocol_version, analyzable.address),
+            TlsHandshakeClientHelloAuthenticationECDSA(protocol_version, analyzable.address),
+            TlsHandshakeClientHelloAuthenticationRarelyUsed(protocol_version, analyzable.address),
+        )
+        for client_hello in client_hello_messsages_in_order_of_probability:
+            try:
+                server_messages = analyzable.do_tls_handshake(
+                    hello_message=client_hello,
+                    last_handshake_message_type=TlsHandshakeType.SERVER_HELLO_DONE
+                )
+            except TlsAlert as e:
+                if e.description == TlsAlertDescription.UNRECOGNIZED_NAME:
+                    break
+                if e.description in [TlsAlertDescription.HANDSHAKE_FAILURE, TlsAlertDescription.INTERNAL_ERROR]:
+                    break
+
+                raise e
+            except NetworkError as e:
+                if e.error != NetworkErrorType.NO_RESPONSE:
+                    raise e
+            except SecurityError:
+                break
+            else:
+                if TlsHandshakeType.CERTIFICATE_REQUEST in server_messages:
+                    distinguished_names = None
+
+                    certificate_request = server_messages[TlsHandshakeType.CERTIFICATE_REQUEST]
+                    if certificate_request.certificate_authorities:
+                        distinguished_names = []
+                        for certificate_authority in certificate_request.certificate_authorities:
+                            try:
+                                distinguished_names.append(
+                                    asn1crypto.x509.Name.load(bytes(bytearray(certificate_authority))).native
+                                )
+                            except ValueError:
+                                pass
+
+                    return (
+                        list(certificate_request.certificate_types),
+                        list(certificate_request.supported_signature_algorithms),
+                        distinguished_names,
+                    )
+
+                break
+
+        return None, None, None
+
+    def analyze(self, analyzable, protocol_version):
+        certificate_types, supported_signature_algorithms, distinguished_names = \
+            self._analyze_requierd_certificates(analyzable, protocol_version)
+
+        return AnalyzerResultPublicKeyRequest(
+            target=AnalyzerTargetTls.from_l7_client(analyzable, protocol_version),
+            certificate_types=certificate_types,
+            supported_signature_algorithms=supported_signature_algorithms,
+            distinguished_names=distinguished_names,
+        )
---
+++ test/tls/test_pubkeyreq.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from collections import OrderedDict
+
+import asn1crypto.x509
+
+from cryptoparser.tls.subprotocol import TlsAlertDescription
+from cryptoparser.tls.version import TlsVersion, TlsProtocolVersionFinal
+
+from cryptolyzer.tls.client import L7ClientTlsBase
+from cryptolyzer.tls.exception import TlsAlert
+from cryptolyzer.tls.pubkeyreq import AnalyzerPublicKeyRequest
+
+from .classes import TestTlsCases, L7ServerTlsTest, L7ServerTlsPlainTextResponse
+
+
+class TestTlsPublicKeyRequest(TestTlsCases.TestTlsBase):
+    @mock.patch.object(
+        L7ClientTlsBase, 'do_tls_handshake',
+        side_effect=TlsAlert(TlsAlertDescription.UNRECOGNIZED_NAME),
+    )
+    def test_error_tls_alert_unrecognized_name(self, _):
+        result = self.get_result('badssl.com', 443)
+
+        self.assertEqual(result.certificate_types, None)
+        self.assertEqual(result.supported_signature_algorithms, None)
+        self.assertEqual(result.distinguished_names, None)
+
+    @mock.patch.object(
+        L7ClientTlsBase, 'do_tls_handshake',
+        side_effect=TlsAlert(TlsAlertDescription.HANDSHAKE_FAILURE)
+    )
+    def test_error_tls_alert_handshake_failure(self, _):
+        result = self.get_result('badssl.com', 443)
+
+        self.assertEqual(result.certificate_types, None)
+        self.assertEqual(result.supported_signature_algorithms, None)
+        self.assertEqual(result.distinguished_names, None)
+
+    @mock.patch.object(
+        asn1crypto.x509.Name, 'load',
+        side_effect=ValueError
+    )
+    def test_error_distinguished_name_cannot_be_loaded(self, _):
+        result = self.get_result('client.badssl.com', 443)
+        self.assertEqual(result.distinguished_names, [])
+
+    @staticmethod
+    def get_result(host, port, protocol_version=TlsProtocolVersionFinal(TlsVersion.TLS1_2), timeout=None):
+        analyzer = AnalyzerPublicKeyRequest()
+        l7_client = L7ClientTlsBase.from_scheme('tls', host, port)
+        analyzer_result = analyzer.analyze(l7_client, protocol_version)
+
+        return analyzer_result
+
+    def test_real_server(self):
+        result = self.get_result('badssl.com', 443)
+        self.assertEqual(result.certificate_types, None)
+        self.assertEqual(result.supported_signature_algorithms, None)
+        self.assertEqual(result.distinguished_names, None)
+
+        result = self.get_result('client.badssl.com', 443)
+        self.assertEqual(
+            result.distinguished_names,
+            [
+                OrderedDict([
+                    ('country_name', 'US'),
+                    ('state_or_province_name', 'California'),
+                    ('locality_name', 'San Francisco'),
+                    ('organization_name', 'BadSSL'),
+                    ('common_name', 'BadSSL Client Root Certificate Authority')
+                ])
+            ]
+        )
+
+    def test_plain_text_response(self):
+        threaded_server = L7ServerTlsTest(
+            L7ServerTlsPlainTextResponse('localhost', 0, timeout=0.2),
+        )
+        threaded_server.start()
+
+        result = self.get_result('localhost', threaded_server.l7_server.l4_transfer.bind_port)
+        self.assertEqual(result.certificate_types, None)
+        self.assertEqual(result.supported_signature_algorithms, None)
+        self.assertEqual(result.distinguished_names, None)
