ec9cd225eb5b934d58c715f0ee2a2317be0fb088
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2023-04-04 00:18:32 +0200

    chore(setup)!: Update project metadata

--- cryptolyzer/__setup__.py
+++ cryptolyzer/__setup__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'CryptoLyzer'
+__technical_name__ = __title__.lower()
 __version__ = '0.8.4'
 __description__ = 'Fast and flexible cryptographic protocol analyzer'
 __author__ = 'Szilárd Pfeiffer'
 __author_email__ = 'coroner@pfeifferszilard.hu'
-__url__ = 'https://gitlab.com/coroner/cryptolyzer'
+__url__ = 'https://gitlab.com/coroner/' + __technical_name__
 __license__ = 'MPL-2.0'
