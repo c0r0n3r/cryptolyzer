e493df5c4ce862f288000fa0dc24811cf426d47d
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2023-10-12 17:03:27 +0200

    chore(release): Bump version number to 0.10.3 and update changelog

--- cryptolyzer/__setup__.py
+++ cryptolyzer/__setup__.py
@@ -2,7 +2,7 @@
 
 __title__ = 'CryptoLyzer'
 __technical_name__ = __title__.lower()
-__version__ = '0.10.2'
+__version__ = '0.10.3'
 __description__ = 'Fast and flexible cryptographic protocol analyzer'
 __author__ = 'Szilárd Pfeiffer'
 __author_email__ = 'coroner@pfeifferszilard.hu'
