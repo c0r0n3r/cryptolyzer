7798f09b57ca9b1871f4f959bf54b26d1eda2e9b
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2023-12-26 20:23:53 +0100

    chore(docs)!: Add docs directory to pep8 and pylint

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,17 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
 
 import datetime
+import os
+import sys
 
-from cryptolyzer.__setup__ import __author__, __title__, __version__
+sys.path.insert(0, os.path.abspath('..'))
+from cryptodatahub.__setup__ import (  # noqa: E402, pylint: disable=wrong-import-position
+    __author__,
+    __title__,
+    __version__,
+)
 
 
 extensions = [
@@ -12,13 +19,12 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon'
 ]
-
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 
 project = __title__
-copyright = f'{datetime.datetime.now().year}, {__author__}'
+copyright = f'{datetime.datetime.now().year}, {__author__}'  # pylint: disable=redefined-builtin
 version = release = __version__
 
 exclude_patterns = ['_build']
