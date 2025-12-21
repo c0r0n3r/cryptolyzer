8c7f8f773107728f7836e875d2d43834375f193a
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2025-12-21 22:47:04 +0100

    chore(python): Add testing with latest (3.14) stable version

--- cryptolyzer/__setup__.py
+++ cryptolyzer/__setup__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import email.utils
 import importlib.metadata
 
 metadata = importlib.metadata.metadata('cryptolyzer')
@@ -8,7 +9,7 @@ __title__ = metadata['Name']
 __technical_name__ = __title__.lower()
 __version__ = metadata['Version']
 __description__ = metadata['Summary']
-__author__ = metadata['Author']
-__author_email__ = metadata['Author-email']
+__author__ = email.utils.parseaddr(metadata['Author-email'])[0]
+__author_email__ = email.utils.parseaddr(metadata['Author-email'])[1]
 __url__ = 'https://gitlab.com/coroner/' + __technical_name__
 __license__ = metadata['License']
