e716a154a5fa40d380fa34886e1f5de7c0ec9e37
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2026-06-20 14:31:17 +0200

    test(tls): Convert all sigalgos tests to offline (#184)

--- test/tls/test_sigalgos.py
+++ test/tls/test_sigalgos.py
@@ -4,9 +4,8 @@
 from unittest import mock
 
 from test.common.classes import TestMainBase
-from test.common.markers import live_dns, live_server
-
 from cryptodatahub.tls.algorithm import TlsSignatureAndHashAlgorithm
+from cryptoparser.tls.extension import TlsExtensionSignatureAlgorithms
 from cryptoparser.tls.subprotocol import TlsAlertDescription
 from cryptoparser.tls.version import TlsVersion, TlsProtocolVersion
 
@@ -14,6 +13,7 @@ from cryptolyzer.common.exception import SecurityError, SecurityErrorType
 from cryptolyzer.common.transfer import L4TransferSocketParams
 from cryptolyzer.tls.client import L7ClientTlsBase
 from cryptolyzer.tls.exception import TlsAlert
+from cryptolyzer.tls.server import L7ServerTls
 from cryptolyzer.tls.sigalgos import AnalyzerSigAlgos
 
 from cryptolyzer.__main__ import main
@@ -36,16 +36,14 @@ class TestTlsSigAlgos(TestTlsCases.TestTlsBase, TestMainBase):
         result = analyzer.analyze(l7_client, protocol_version)
         return result
 
-    @live_dns
     @mock.patch.object(
         L7ClientTlsBase, 'do_tls_handshake',
         side_effect=TlsAlert(TlsAlertDescription.PROTOCOL_VERSION)
     )
     def test_error_tls_alert_protocol_version(self, _):
-        result = self.get_result('ecc256.badssl.com', 443, l4_socket_params=L4TransferSocketParams(timeout=10))
+        result = self.get_result('localhost', 0)
         self.assertEqual(result.sig_algos, [])
 
-    @live_dns
     @mock.patch.object(
         L7ClientTlsBase, 'do_tls_handshake',
         side_effect=[
@@ -61,21 +59,31 @@ class TestTlsSigAlgos(TestTlsCases.TestTlsBase, TestMainBase):
         ]
     )
     def test_error_response_error_no_response(self, _):
-        result = self.get_result('ecc256.badssl.com', 443, l4_socket_params=L4TransferSocketParams(timeout=10))
+        result = self.get_result('localhost', 0)
         self.assertEqual(result.sig_algos, [
             TlsSignatureAndHashAlgorithm.RSA_NONE,
         ])
 
-    @live_server
     def test_sigalgos(self):
-        result = self.get_result('badssl.com', 443, l4_socket_params=L4TransferSocketParams(timeout=10))
-        self.assertEqual(result.sig_algos, [
+        accepted_sig_algos = [
             TlsSignatureAndHashAlgorithm.RSA_SHA1,
             TlsSignatureAndHashAlgorithm.RSA_SHA224,
             TlsSignatureAndHashAlgorithm.RSA_SHA256,
             TlsSignatureAndHashAlgorithm.RSA_SHA384,
             TlsSignatureAndHashAlgorithm.RSA_SHA512,
-        ])
+        ]
+
+        def side_effect(client_hello):
+            extension = client_hello.extensions.get_item_by_type(
+                TlsExtensionSignatureAlgorithms.get_extension_type()
+            )
+            if extension.hash_and_signature_algorithms[0] not in accepted_sig_algos:
+                raise TlsAlert(TlsAlertDescription.HANDSHAKE_FAILURE)
+
+        with mock.patch.object(L7ClientTlsBase, 'do_tls_handshake', side_effect=side_effect):
+            result = self.get_result('localhost', 0)
+
+        self.assertEqual(result.sig_algos, accepted_sig_algos)
         log_lines = self.get_log_lines()
         for idx, signature_algorithm in enumerate(result.sig_algos):
             self.assertEqual(f'Server offers signature algorithm {signature_algorithm.name}', log_lines[idx])
@@ -91,15 +99,20 @@ class TestTlsSigAlgos(TestTlsCases.TestTlsBase, TestMainBase):
             []
         )
 
-    @live_server
     def test_json(self):
-        result = self.get_result('ecc256.badssl.com', 443, l4_socket_params=L4TransferSocketParams(timeout=10))
+        threaded_server = self.create_server()
+        result = self.get_result('localhost', threaded_server.l7_server.l4_transfer.bind_port)
         self.assertTrue(result)
 
-    @live_server
     def test_output(self):
+        threaded_server = L7ServerTlsTest(L7ServerTls(
+            '127.0.0.1', 0,
+            L4TransferSocketParams(timeout=5.0),
+        ))
+        threaded_server.wait_for_server_listen()
         func_arguments, cli_arguments = self._get_arguments(
-            TlsProtocolVersion(TlsVersion.TLS1_2), 'sigalgos', 'ecc256.badssl.com', 443, timeout=10, scheme='tls'
+            TlsProtocolVersion(TlsVersion.TLS1_2), 'sigalgos', '127.0.0.1',
+            threaded_server.l7_server.l4_transfer.bind_port, scheme='tls'
         )
         result = self.get_result(**func_arguments)
         self.assertEqual(self._get_test_analyzer_result_json(**cli_arguments), result.as_json() + '\n')
